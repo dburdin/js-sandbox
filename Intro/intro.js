2d4ac3a60ae7ceee38a091281194d15deb038130
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-09-20 21:22:30 +0300

    1

---
+++ Intro/intro.js
@@ -0,0 +1,51 @@
+//* pre-increment, post-increment
+// let a = 5;
+// let b = ++a;
+// console.log(`++a: a:${a}, b:${b}`);
+
+// let c = 5;
+// let d = a++;
+// console.log(`a++: c:${c}, d:${d}`);
+//* optional chaining
+// const person = {
+//   name: "Alice",
+//   address: {},
+// };
+
+// const city = person.address?.city;
+// console.log(city);
+
+//* Nullish Coalescing Operator
+// const userCount = null;
+// const defaultCount = 10;
+// const totalUsers = userCount ?? defaultCount;
+
+//* loops
+let newString = "";
+let num = 0;
+const string = "abracadabra";
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const object = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+// for (let i = 0; i < string.length; i++) {
+//   console.log(string[i]);
+// }
+// for (const letter of string) {
+//   console.log(letter);
+// }
+// for (const key in string) {
+//   console.log(key);
+// }
+// while (num < 10) {
+//   console.log("hello");
+//   num += 1;
+// }
+// do {
+//   console.log("hello");
+//   num += 1;
+// } while (num === 10);
