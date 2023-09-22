e837b8e962125bdb1c20760970c83fb279444821
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-09-22 12:55:27 +0300

    4

---
+++ eventLoop/eventLoop4.js
@@ -0,0 +1,48 @@
+// console.log("1");
+
+// const promise = new Promise((resolve, reject) => {
+//   const canFulfill = Math.random() > 0.5;
+//   if (canFulfill) {
+//     resolve("3");
+//   } else {
+//     reject("4");
+//   }
+// });
+
+// promise.then((result) => console.log(result)).catch((result) => console.log(result));
+
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then((data) => console.log(data));
+// console.log(
+//   fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((response) => console.log(response))
+// );
+
+// setTimeout(() => {
+//   console.log(25);
+// }, 0);
+
+// console.log("2");
+
+// setTimeout(() => console.log("timeout"));
+
+// Promise.resolve().then(() => console.log("promise"));
+
+// console.log("code");
+
+// console.log(1);
+
+// setTimeout(() => console.log(2));
+
+// Promise.resolve().then(() => console.log(3));
+
+// Promise.resolve().then(() => setTimeout(() => console.log(4)));
+
+// Promise.resolve().then(() => console.log(5));
+
+// setTimeout(() => console.log(6));
+
+// console.log(7);
