7800439d9c47b68bfa715e85f13e5ab646046eca
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2024-04-17 21:34:00 +0300

    case 2

--- playground.js
+++ playground.js
@@ -1,32 +1,3 @@
-for (let i = 0; i < 7; i++) {
-  let symbol = "";
-
-  switch (i) {
-    case 0:
-      symbol += "#";
-      break;
-    case 1:
-      symbol += "##";
-      break;
-    case 2:
-      symbol += "###";
-      break;
-    case 3:
-      symbol += "####";
-      break;
-    case 4:
-      symbol += "#####";
-      break;
-    case 5:
-      symbol += "######";
-      break;
-    case 6:
-      symbol += "#######";
-      break;
-
-    default:
-      break;
-  }
-
-  console.log(symbol);
+for (let i = "#"; i.length < 8; i += "#") {
+  console.log(i);
 }
